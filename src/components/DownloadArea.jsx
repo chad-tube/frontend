cb451a3f6f5bdad1515ab95aad58e92493604740
StanMathers <sabagamgebeli@gmail.com>
2023-12-05 21:00:31 +0400

    Added type choosing states. Added Type.jsx component

--- src/components/DownloadArea.jsx
+++ src/components/DownloadArea.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Input from "./Input";
 
-const DownloadArea = ({ onDownloadClick }) => {
+const DownloadArea = ({ onDownloadClick, typeChoices, chosenType, onTypeChange }) => {
 
 	return (
 		<div>
 			<div className="d-flex flex-column text-center mt-5 gap-4">
 				<h4 className="h4">YouTube Video Downloader</h4>
-				<Input onDownloadClick={onDownloadClick} />
+				<Input onDownloadClick={onDownloadClick} typeChoices={typeChoices} chosenType={chosenType} onTypeChange={onTypeChange} />
 			</div>
 		</div>
 	);
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,21 @@
 import React, { useRef } from "react";
+import Type from "./Type";
 
-const Input = ({ onDownloadClick }) => {
+const Input = ({ onDownloadClick, typeChoices, chosenType, onTypeChange }) => {
 	const urlRef = useRef(null);
 
 	return (
 		<div className="row gy-2 px-md-5">
-			<div className="col-sm-10 col-md-11">
+			<div className="col-sm-10 col-md-10">
 				<input
 					type="text"
 					className="form-control"
 					placeholder="Enter YouTube URL"
 					ref={urlRef}
 				/>
+			</div>
+			<div className="col-sm-2 col-md-1">
+				<Type typeChoices={typeChoices} chosenType={chosenType} onTypeChange={onTypeChange} />
 			</div>
 			<div className="col-sm-2 col-md-1">
 				<button
