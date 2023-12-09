8fbb945b48056cbb9bf115bd9b4bc5a2d721e61c
StanMathers <sabagamgebeli@gmail.com>
2023-12-09 21:24:48 +0400

    Removed jquery from vendor, replaced it with npm installed jquery. Included installed jquery in index.html as sb-admin depends on jquery

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import "./assets/src/css/sb-admin-2.min.css";
-import "./assets/src/vendor/jquery/jquery.min.js";
 import "./assets/src/js/sb-admin-2.min.js";
 
 import "./assets/src/vendor/bootstrap/js/bootstrap.bundle.min.js";
